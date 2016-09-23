dbbdadb2f4d650a3fca89b9762db3b1188af3b04
jeong da seul <ds1417@naver.com>
2016-09-23 12:35:58 +0900

    파일 수정

---
+++ B01.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class B01.java{
+ 
+ public static void main(String[] args){
+
+ Scanner input = new Scanner(System.in);
+ System.out.print("Input your birth year:");
+ int year = s.nextInt();
+ this.age =  2016 - year -1;
+
+
+ void print(){
+ if(this.age >= 20){
+
