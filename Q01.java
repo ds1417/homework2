652942e24a066ae2c82c9f2b9286cddebb740c64
jeong da seul <ds1417@naver.com>
2016-09-30 01:13:25 +0900

    파일작성

---
+++ Q01.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Q01{
+ 
+ public static void main(String[] args){
+  
+  Scanner input = new Scanner(System.in);
+   
+   int birth_year;
+   int age;
+    
+    System.out.print("birthyear:");
+    coffee = input.nextInt();
+     
+
+     age = 2016- birth_year +1
+
+     if(age<20) {
+     system.ot.print("under 20");}
+     else (age>20) {
+     system.out.print("up 20");}
+     }
+
